37ed14b843ffa314e3d3ce37870f4039c71ed039
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-09-18 23:57:13 +0545

    View Applicants + Tracking Application

--- src/api/apiApplication.js
+++ src/api/apiApplication.js
@@ -32,3 +32,18 @@ export async function applyToJob(token, _, jobData) {
 
   return data;
 }
+
+export async function updateApplicationStatus(token, { job_id }, status) {
+  const supabase = await supabaseClient(token);
+  const { data, error} = await supabase
+  .from("applications")
+  .update({ status })
+  .eq("job_id", job_id)
+  .select();
+
+  if(error || data.length === 0) {
+    console.error("Error Updating Application Status:", error);
+    return null;
+  }
+  return data;
+}
--- src/pages/job.jsx
+++ src/pages/job.jsx
@@ -8,6 +8,7 @@ import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import { BarLoader } from 'react-spinners'
 import {ApplyJobDrawer} from '@/components/applyjob';
+import ApplicationCard from '@/components/application-card';
 
 const JobPage = () => {
 
@@ -103,6 +104,16 @@ const JobPage = () => {
         applied= {job?.applications?.find((ap) => ap.candidate_id === user.id)}
       />
      )}
+     
+{job?.applications?.length > 0 && job?.recruiter_id === user?.id && (
+  <div className='flex flex-col gap-2'>
+    <h2 className='text-2xl sm:text-3xl font-bold'>Applications</h2>
+    { job?.applications.map((application) => {
+      return <ApplicationCard key={application.id} application={application} />
+     })}
+  </div>
+)}
+     
 
     </div>
   )
