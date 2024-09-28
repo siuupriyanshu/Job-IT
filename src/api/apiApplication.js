cc416c0303adceafb0affd45e79b3dc179408d44
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-09-28 21:35:02 +0545

    My applications page

--- src/api/apiApplication.js
+++ src/api/apiApplication.js
@@ -47,3 +47,18 @@ export async function updateApplicationStatus(token, { job_id }, status) {
   }
   return data;
 }
+
+export async function getApplications(token, { user_id }) {
+  const supabase = await supabaseClient(token);
+  const { data, error } = await supabase
+    .from("applications")
+    .select("*, job:jobs(title, company:companies(name))")
+    .eq("candidate_id", user_id);
+
+  if (error) {
+    console.error("Error fetching Applications:", error);
+    return null;
+  }
+
+  return data;
+}
